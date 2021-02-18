d0d339e6d376c23c26a026ffe8dce024ad3fa1f3
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-02-18 12:51:44 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -8,17 +8,17 @@ const App = ()=> {
 
     return(<div className="App component">
         <h1>Main App</h1>
-        <PersonContext.Provider value={[person, setPerson, "asdfasfs"]}>
+        <PersonContext.Provider value={[person, setPerson, "This is a random string"]}>
             <SubComp1/>
         </PersonContext.Provider>
     </div>);
 };
 
 const SubComp1 = ()=> {
-    const [ person ] = useContext(PersonContext);
+    const [ person, setPerson, randomString] = useContext(PersonContext);
 
     return(<div className="component">
-        <h2>Sub Component 1</h2>
+        <h2>Sub Component 1 : {randomString}</h2>
         <h3>Name: {person.name.title} {person.name.first} {person.name.last}</h3>
 
         <SubComp2 />
