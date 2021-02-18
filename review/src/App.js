f510a22ae504887d99dc48cdefc2fe918bb2e9e5
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-02-18 12:35:55 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -3,7 +3,10 @@ import data from './data';
 
 
 const App = ()=> {
-    const [person, setPerson ]
+    const [person, setPerson ] = useState(data);
+
+    console.log(person);
+    
     return(<div className="App component">
         <h1>Main App</h1>
         <SubComp1 />
