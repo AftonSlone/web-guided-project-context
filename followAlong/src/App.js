f1dfcc7c811d07ec4a6b4c7e05e7671fbe90f2ef
Afton Slone <afton.slone@gmail.com>
2021-02-23 05:56:24 -0500

    Finished Guided Project

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -1,30 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useContext, createContext } from "react";
 
 import { data } from "./data";
 
 import FamilyTree from "./components/FamilyTree";
 import "./styles.scss";
 
+export const FamilyContext = createContext();
+
 export default function App() {
   const [families] = useState(data);
   const [activeFamily, setActiveFamily] = useState(families[0]);
 
   return (
-    <div className="App">
-      <section className="header">
-        <h1>Family Trees</h1>
-        {families.map(d => (
-          <button
-            className={`family-button ${d.familyName ===
-              activeFamily.familyName && "active"}`}
-            key={d.familyName}
-            onClick={() => setActiveFamily(d)}
-          >
-            {d.familyName}
-          </button>
-        ))}
-      </section>
-      {activeFamily && <FamilyTree family={activeFamily} />}
-    </div>
+    <FamilyContext.Provider value={activeFamily}>
+      <div className="App">
+        <section className="header">
+          <h1>Family Trees</h1>
+          {families.map((d) => (
+            <button
+              className={`family-button ${
+                d.familyName === activeFamily.familyName && "active"
+              }`}
+              key={d.familyName}
+              onClick={() => setActiveFamily(d)}
+            >
+              {d.familyName}
+            </button>
+          ))}
+        </section>
+        {activeFamily && <FamilyTree />}
+      </div>
+    </FamilyContext.Provider>
   );
 }
--- review/src/App.js
+++ review/src/App.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState, useContext, createContext, useReducer } from "react";
+import { reducer, setLocation, setName } from "./reducer";
+import data from "./data";
 
-const App = ()=> {
-    return(<h1>Hello World</h1>);
+const PersonContext = createContext();
+
+export default function App() {
+  const [person, dispatch] = useReducer(reducer, data);
+  return (
+    <div className="component">
+      <h1>APP COMPONENT</h1>
+      <PersonContext.Provider value={[person, setName, setLocation, dispatch]}>
+        <SubComponent1 />
+      </PersonContext.Provider>
+    </div>
+  );
+}
+
+const SubComponent1 = () => {
+  return (
+    <div className="component">
+      <h2>SUBCOMPONENT 1</h2>
+      <SubComponent2 />
+    </div>
+  );
 };
 
-export default App;
+const SubComponent2 = () => {
+  const [person] = useContext(PersonContext);
+  return (
+    <div className="component">
+      <h2>SUBCOMPONENT 2</h2>
+      <p>
+        Name: {person.name.title} {person.name.first} {person.name.last}
+      </p>
+      <SubComponent3 />
+    </div>
+  );
+};
+
+const SubComponent3 = () => {
+  const [person, setName, setLocation, dispatch] = useContext(PersonContext);
+
+  const handleLocation = () => {
+    dispatch(setLocation("24 Left Beaver Creek RD", "Minnie", "KY"));
+  };
+
+  const handleName = () => {
+    dispatch(setName("Mr", "Afton", "Slone"));
+  };
+
+  return (
+    <div className="component">
+      <h2>SUBCOMPONENT 3</h2>
+      <p>
+        Location: {person.location.street} {person.location.city}{" "}
+        {person.location.state}
+      </p>
+      <button onClick={handleLocation}>Change Location</button>
+      <button onClick={handleName}>Change Name</button>
+    </div>
+  );
+};
